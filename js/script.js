730dc6a3d532be587e4db12a088c85e36804dd9a
AndreiDaraban <72549078+andrei-db@users.noreply.github.com>
2023-06-26 15:40:09 +0300

    page navigation

--- js/script.js
+++ js/script.js
@@ -1,42 +1,62 @@
 
 var headerTitleDiv = document.getElementById("header-title");
-  window.addEventListener("DOMContentLoaded", function() {
-   
-    headerTitleDiv.addEventListener("animationend", activateBounceAnimation);
-  });
-  
-  function activateBounceAnimation() {
-   
-    headerTitleDiv.classList.add("bounce");
-    headerTitleDiv.addEventListener("animationiteration", stopBounceAnimation);
-    headerTitleDiv.removeEventListener("animationend", activateBounceAnimation);
-  }
-  
-  function stopBounceAnimation(event) {
-    
-    if (event.animationName === "bounce" && event.elapsedTime >= 1) {
-        headerTitleDiv.classList.remove("bounce");
-        headerTitleDiv.classList.remove("slide-from-top");
-        headerTitleDiv.removeEventListener("animationiteration", stopBounceAnimation);
-    }
-  }
+window.addEventListener("DOMContentLoaded", function () {
 
-  var menu_btn=document.querySelector('#menu-btn');
-  var container=document.querySelector('.container');
-  var navbar=document.getElementById('navbar');
+  headerTitleDiv.addEventListener("animationend", activateBounceAnimation);
+});
 
-  menu_btn.addEventListener("click",activateMenu);
+function activateBounceAnimation() {
 
-function activateMenu(){
-    if(menu_btn.innerHTML=="Menu"){
-      menu_btn.innerHTML="Close";
-      container.classList.add("container-rotate");
-      navbar.style.display="flex";
-      
-    }
-    else{
-      menu_btn.innerHTML="Menu";
-      container.classList.remove("container-rotate");
-      navbar.style.display="none";
-    }
+  headerTitleDiv.classList.add("bounce");
+  headerTitleDiv.addEventListener("animationiteration", stopBounceAnimation);
+  headerTitleDiv.removeEventListener("animationend", activateBounceAnimation);
 }
+
+function stopBounceAnimation(event) {
+
+  if (event.animationName === "bounce" && event.elapsedTime >= 1) {
+    headerTitleDiv.classList.remove("bounce");
+    headerTitleDiv.classList.remove("slide-from-top");
+    headerTitleDiv.removeEventListener("animationiteration", stopBounceAnimation);
+  }
+}
+
+var menu_btn = document.querySelector('#menu-btn');
+var container = document.querySelector('.container');
+var navbar = document.getElementById('navbar');
+const page = document.querySelector('.page');
+
+menu_btn.addEventListener('click',enableDisableMenu );
+
+function enableDisableMenu(){
+
+  if(menu_btn.innerHTML=="Menu"){
+    menu_btn.innerHTML="Close";
+    container.classList.toggle('perspective');
+    page.classList.toggle('perspective');
+    navbar.classList.toggle('show');
+
+
+  }else{
+    menu_btn.innerHTML="Menu";
+    navbar.classList.remove('show');
+    page.classList.remove('perspective');
+
+  }
+}
+
+const links = document.querySelectorAll('#navbar a[href^="#"]');
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const target = document.querySelector(link.getAttribute('href'));
+
+    if (target) {
+      page.scrollTo({
+        top:  target.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
