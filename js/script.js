fdf561877b7d2946b145427092f97411e6e1cb75
AndreiDaraban <72549078+andrei-db@users.noreply.github.com>
2023-07-26 04:07:32 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 
-var change_btn=document.getElementById('change-theme');
-var css_file=document.getElementById('css-file');
-change_btn.addEventListener('click',()=>{
-  if(css_file.href.endsWith("dark-theme.css")){
-    css_file.href="css/light-theme.css";
-    change_btn.innerHTML="Dark Theme";
-  }else{
-    css_file.href="css/dark-theme.css";
-    change_btn.innerHTML="Light Theme";
+var change_btn = document.getElementById('change-theme');
+var css_file = document.getElementById('css-file');
+change_btn.addEventListener('click', () => {
+  if (css_file.href.endsWith("dark-theme.css")) {
+    css_file.href = "css/light-theme.css";
+    change_btn.innerHTML = "Dark Theme";
+  } else {
+    css_file.href = "css/dark-theme.css";
+    change_btn.innerHTML = "Light Theme";
   }
 });
 
@@ -40,13 +40,13 @@ function enableDisableMenu() {
     container.classList.toggle('perspective');
     page.classList.toggle('perspective');
     navbar.classList.toggle('show');
-   
+
 
   } else {
     menu_btn.innerHTML = "Menu";
     navbar.classList.remove('show');
     page.classList.remove('perspective');
-    
+
   }
 }
 
@@ -85,9 +85,68 @@ linksFooter.forEach(link => {
 
 const questions = document.querySelectorAll('.question');
 const answers = document.querySelectorAll('.answer');
-for(let i=0;i<questions.length;i++){
-    questions[i].addEventListener('click',function(){
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener('click', function () {
 
-      answers[i].classList.toggle('show-answer');
-    });
+    answers[i].classList.toggle('show-answer');
+  });
 }
+
+var leftProject = document.querySelector('.left-button');
+var rightProject = document.querySelector('.right-button');
+var projectsContainer = document.querySelector('#our-projects .projects-container');
+var projects = document.querySelectorAll('#our-projects .projects-container .item');
+
+const dreapta = "translate3d(550px,0,0)";
+const stanga = "translate3d(-550px,0,0)";
+const fata = "translate3d(0,0,300px)";
+const spate = "translate3d(0,0,-300px)";
+
+
+choose();
+leftProject.addEventListener('click', choose);
+
+rightProject.addEventListener('click', choose2);
+
+function choose() {
+  for (let i = 0; i < 4; i++) {
+    if (projects[i].dataset.val == "stanga") {
+      projects[i].style.transform = fata;
+      projects[i].dataset.val = "fata";
+    }
+    else if (projects[i].dataset.val == "fata") {
+      projects[i].style.transform = dreapta;
+      projects[i].dataset.val = "dreapta";
+    }
+    else if (projects[i].dataset.val == "dreapta") {
+      projects[i].style.transform = spate;
+      projects[i].dataset.val = "spate";
+    }
+    else if (projects[i].dataset.val == "spate") {
+      projects[i].style.transform = stanga;
+      projects[i].dataset.val = "stanga";
+    }
+  }
+}
+function choose2() {
+  for (let i = 0; i < 4; i++) {
+    if (projects[i].dataset.val == "stanga") {
+      projects[i].style.transform = spate;
+      projects[i].dataset.val = "spate";
+    }
+    else if (projects[i].dataset.val == "fata") {
+      projects[i].style.transform = stanga;
+      projects[i].dataset.val = "stanga";
+    }
+    else if (projects[i].dataset.val == "dreapta") {
+      projects[i].style.transform = fata;
+      projects[i].dataset.val = "fata";
+    }
+    else if (projects[i].dataset.val == "spate") {
+      projects[i].style.transform = dreapta;
+      projects[i].dataset.val = "dreapta";
+    }
+  }
+
+}
+
